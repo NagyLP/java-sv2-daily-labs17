3b31810b548f000214fa45f4c2b2630bd5af0836
NagyLP <nagy.peter@pr.hu>
2022-02-16 01:37:38 +0100

    JDBC - Movies-Actors

--- src/main/java/Week17/day02/Movie.java
+++ src/main/java/Week17/day02/Movie.java
@@ -1,4 +1,36 @@
 package Week17.day02;
 
+import java.time.LocalDate;
+
 public class Movie {
+
+    private long id;
+    private String title;
+    private LocalDate release_date;
+
+    public Movie(long id, String title, LocalDate release_date) {
+        this.id = id;
+        this.title = title;
+        this.release_date = release_date;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie: " +
+                "\n  id: " + id +
+                "\n  title: '" + title + '\'' +
+                "\n  release_date: " + release_date;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDate getRelease_date() {
+        return release_date;
+    }
 }
--- src/main/java/Week17/day02/MoviesRespository.java
+++ src/main/java/Week17/day02/MoviesRespository.java
@@ -1,7 +1,6 @@
 package Week17.day02;
 
 import javax.sql.DataSource;
-import javax.swing.*;
 import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -9,37 +8,36 @@ import java.util.List;
 
 public class MoviesRespository {
 
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
     public MoviesRespository(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
 
-    public void saveActor(String title, LocalDate relasedate) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement stmt = connection.prepareStatement("insert into movies(title, relase_date) values(?,?)")) {
+    public void saveMovie(String title, LocalDate relasedate) {
+        try (Connection connection =
+                     dataSource.getConnection();
+             PreparedStatement stmt =
+                     connection.prepareStatement("insert into movies(title, release_date) values(?,?)")) {
             stmt.setString(1, title);
             stmt.setDate(2, Date.valueOf(relasedate));
             stmt.executeUpdate();
         } catch (SQLException sqle) {
-            throw new IllegalStateException("Connect ERROR...", sqle);
+            throw new IllegalStateException("Connection ERROR...", sqle);
         }
     }
 
-    public List<Movie> findAllMovies(String movie) {
-        List<Movie> movies = new ArrayList<>();
-
-        try (Connection connection = dataSource.getConnection();
+    public List<Movie> findAllMovies() {
+        try (ResultSet resultSet = dataSource.getConnection().createStatement().executeQuery("SELECT * FROM movies")) {
 // ELEGENDŐ a Statement, nem kell PrepareState..., mert nem paraméterezett)
-             Statement stmt = connection.prepareStatement("SELECT * FROM movies")) {
-            ResultSet rs = stmt.executeQuery()){
-            }
+            return processResultSet(resultSet);
+        } catch (SQLException sqle) {
+            throw new IllegalStateException("Connection ERROR...", sqle);
         }
-        return processResultSet();
     }
 
-    private List<Movie> processResultSet(ResultSet rs) throws Exception {
+    private List<Movie> processResultSet(ResultSet rs) throws SQLException {
         List<Movie> movies = new ArrayList<>();
         while (rs.next()) {
             long id = rs.getLong("id");
@@ -47,7 +45,6 @@ public class MoviesRespository {
             LocalDate relaseDate = rs.getDate("release_date").toLocalDate();
             movies.add(new Movie(id, title, relaseDate));
         }
-
+        return movies;
     }
-
 }
